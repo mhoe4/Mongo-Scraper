aa5b9e5493496ff2823acdfe169284cab3e81596
mhoef <mhoefer1@gmail.com>
2019-03-30 16:33:54 -0400

    mongoose connection

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
 // Routes
 app.get("/", function(req, res) {
